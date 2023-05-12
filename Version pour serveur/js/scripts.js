a067b54b733194e26c79b5864b96cbfc0bf7e334
Jimmy Mrti <112329444+JimmyMrti@users.noreply.github.com>
2023-05-12 19:53:04 +0200

    Add files via upload

---
+++ Version pour serveur/js/scripts.js
@@ -0,0 +1,160 @@
+/* Description: Custom JS file */
+
+/* Navigation*/
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
+
+window.onload = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 30) {
+		document.getElementById("navbarExample").classList.add("top-nav-collapse");
+	} else if ( document.documentElement.scrollTop < 30 ) {
+		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+	}
+}
+
+// Navbar on mobile
+let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
+
+for (let i = 0; i < elements.length; i++) {
+	elements[i].addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+	});
+}
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
+});
+
+
+
+// Hover on desktop
+function toggleDropdown(e) {
+	const _d = e.target.closest(".dropdown");
+	let _m = document.querySelector(".dropdown-menu", _d);
+
+	setTimeout(
+		function () {
+		const shouldOpen = _d.matches(":hover");
+		_m.classList.toggle("show", shouldOpen);
+		_d.classList.toggle("show", shouldOpen);
+
+		_d.setAttribute("aria-expanded", shouldOpen);
+		},
+		e.type === "mouseleave" ? 300 : 0
+	);
+}
+
+// On hover
+const dropdownCheck = document.querySelector('.dropdown');
+
+if (dropdownCheck !== null) { 
+	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+
+	// On click
+	document.querySelector(".dropdown").addEventListener("click", (e) => {
+		const _d = e.target.closest(".dropdown");
+		let _m = document.querySelector(".dropdown-menu", _d);
+		if (_d.classList.contains("show")) {
+			_m.classList.remove("show");
+			_d.classList.remove("show");
+		} else {
+			_m.classList.add("show");
+			_d.classList.add("show");
+		}
+	});
+}
+  
+
+/* Card Slider - Swiper */
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
+
+
+/* Back To Top Button */
+// Get the button
+myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // for Safari
+	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+}
+
+
+
+//------------------
+const bannerContent = document.querySelectorAll('.banner-content');
+const bannerNav = document.querySelectorAll('.banner-nav button');
+
+let currentIndex = 0;
+let intervalId;
+
+function showImage(index) {
+  bannerContent[currentIndex].classList.remove('active');
+  bannerNav[currentIndex].classList.remove('active');
+
+  bannerContent[index].classList.add('active');
+  bannerNav[index].classList.add('active');
+
+  currentIndex = index;
+}
+
+function startInterval() {
+  intervalId = setInterval(() => {
+    let nextIndex = currentIndex + 1;
+
+    if (nextIndex >= bannerContent.length) {
+      nextIndex = 0;
+    }
+
+    showImage(nextIndex);
+  }, 3000);
+}
+
+startInterval();
+
+bannerNav.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    clearInterval(intervalId);
+    showImage(index);
+    startInterval();
+  });
+});
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const slideCount = slides.length;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slideCount;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(nextSlide, 3000);
